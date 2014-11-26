3b383b2941d1bd0555dee7206d5f41f6e4088df7
Carlos Hernandez <honchkrow1995@gmail.com>
2014-11-26 01:39:07 -0600

    Allow to insert values in memory section

--- ARM/MainWindow.xaml.cs
+++ ARM/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,35 @@ namespace ARM
     /// </summary>
     public partial class MainWindow : Window
     {
+        //global variables
+        //ObservableCollection<Instruction> Instructions = new ObservableCollection<Instruction>();
+        int dirCount = 0;
+        int dataCount = 16;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+        
+        private void btnAddInstruction_Click(object sender, RoutedEventArgs e)
+        {
+            string cbi = ((ComboBoxItem)cboOperator.SelectedItem).Content.ToString();
+            //Instructions.Add (new Instruction() { memPath = count, Operator = cbi, Operand = txtOperand.Text });
+            Instruction _this = new Instruction() { memPath1 = dirCount, memPath2 = dataCount, Operator = cbi, Operand = txtOperand.Text, Value = 0 };
+            grdInstructions.Items.Add(_this);
+            grdData.Items.Add( _this);
+            dirCount++; dataCount++;
+            //grdData.ItemsSource = data;
+        }
+
+        public class Instruction 
+        {
+            public int memPath1 { get; set; }
+            public int memPath2 { get; set; }
+            public string Operator { get; set; }
+            public string Operand { get; set;}
+            public int Value { get; set; }
+        }
+
     }
 }
